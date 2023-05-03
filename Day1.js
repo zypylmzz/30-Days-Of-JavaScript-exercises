e08f3ff8cec71e6be73edeb73b821901be3ab3b5
Zeynep Yılmaz <75902079+zypylmzz@users.noreply.github.com>
2023-05-03 22:53:28 +0300

    Add files via upload
    Solutions of Day 1 workouts

---
+++ Day1.js
@@ -0,0 +1,143 @@
+/* 
+1.Write a single line comment which says, comments can make code readable.[EN]
+// Write a single line comment which says. 
+
+1."Yorumlar, kodu daha okunabilir kılar" yazan tek satırlık bir yorum yazın.[TR]
+// Yorumlar, kodu daha okunabilir kılar. 
+
+2.Write another single comment which says, Welcome to 30DaysOfJavaScript. [EN]
+// Welcome to 30DaysOfJavaScript.
+
+2."30 Günde Javascript" yazan bir başka yorum yazın.[TR] 
+// 30 Günde Javascript. 
+
+3.Write a multiline comment which says, comments can make code readable, easy to reuse and informative. [EN]
+// Comments can make code readable, easy to reuse and informative. 
+
+3."Yorumlar, kodu daha okunabilir kılar, yeniden kullanmayı kolaylaştırır ve bilgilendiricidir." yazan çok satırlı bir yorum yazın. [TR]
+//Yorumlar, kodu daha okunablir kılar,yeniden kullanmayı kolaylaştırır ve bilgilendiricidir.
+
+4.Create a variable.js file and declare variables and assign string, boolean, undefined and null data types. [EN]
+
+variable.js
+let name = "Zeynep"
+let isMarried = false
+let age // If no value is assigned, it is set to undefined.
+let weight = null
+
+4."variable.js" adında bir javascript dosyası oluşturun ve "string", "boolean", "undefined" ve "null" veri türünde değişkenler tanımlayın. [TR]
+
+variable.js
+let isim = "Zeynep"
+let evliMi = false
+let yaş // değer atanmamışsa undefined olarak belirlenir.
+let kilo = null
+
+
+5.Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable. [EN]
+datatypes.js
+let name = "Zeynep"
+let isMarried = false
+let age // If no value is assigned, it is set to undefined.
+let weight = null
+
+console.log(typeof name)
+console.log(typeof isMarried)
+console.log(typeof age)
+console.log(typeof weight)
+
+
+5."datatypes.js" adında bir javascript dosyası oluşturun ve yukarıdaki değişkenlerin veri türlerini typeof kullanarak kontrol edin. [TR]
+
+datatypes.js
+let isim = "Zeynep"
+let evliMi = false
+let yaş // değer atanmamışsa undefined olarak belirlenir.
+let kilo = null
+
+console.log(typeof isim)
+console.log(typeof evliMi)
+console.log(typeof yaş)
+console.log(typeof kilo)
+
+6.Declare four variables without assigning values.[EN]
+
+let variable1
+let variable2
+let varitable3
+let variable4
+
+
+6.Değer atamadan dört farklı değişken tanımlayın.[TR]
+
+let değişken1
+let değişken2
+let değişken3
+let değişken4
+
+
+7.Declare four variables with assigned values. [EN]
+
+let variable1 = null
+let variable2 = 'Welcome to 30DaysOfJavaScript.'
+let varitable3 = 2023
+let variable4 = true
+
+
+7.Değer atayarak dört farklı değişken tanımlayın.[TR]
+
+let değişken1 = null
+let değişken2 = '30 Günde Javascript.'
+let değişken3 = 2023
+let değişken4 = true
+
+
+8.Declare variables to store your first name, last name, marital status, country and age in multiple lines. [EN]
+
+let name = 'Zeynep'
+let surname = 'Yilmaz'
+let mariage = false
+let country = 'Türkiye'
+let age = 22
+
+8.Adınızı, soyadınızı, medeni durumunuzu, ülkenizi ve yaşınnızı tek tek değişken oluşturarak tanımlayın.[TR]
+
+let isim = 'Zeynep'
+let soyadı = 'Yılmaz'
+let evliMi = false
+let ülke = 'Türkiye'
+let yaş = 22
+
+9.Declare variables to store your first name, last name, marital status, country and age in a single line. [EN]
+let name = 'Zeynep',
+    surname = 'Yilmaz',
+    mariage = false,
+    country = 'Türkiye',
+    age = 22
+
+9.Adınızı, soyadınızı, medeni durumunuzu, ülkenizi ve yaşınnızı tek seferde değişken oluşturarak tanımlayın. [TR]
+
+let isim = 'Zeynep',
+    soyadı = 'Yılmaz',
+    evliMi = false,
+    ülke = 'Türkiye',
+    yaş = 22
+
+
+10.Declare two variables "myAg"e and "yourAge" and assign them initial values and log to the browser console. [EN]
+let myAge = 22,
+    yourAge = 23
+
+console.log(`i am ${myAge} years old`);
+console.log(`you are ${yourAge} years old`);
+
+
+10."myAge" ve "yourAge" adında iki değişken tanımlayıp değerlerini belirleyin ve aşağıdaki şekilde konsola log basın. [TR]
+
+let myAge = 22,
+    yourAge = 23
+
+console.log(`Ben ${myAge} yaşındayım.`);
+console.log(`Sen ${yourAge} yaşındasın.`);
+
+*/
